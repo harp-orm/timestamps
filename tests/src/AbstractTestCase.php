3f7c3046778c2cd35e5bd53e9f866571a6372e34
Ivan Kerin <ikerin@gmail.com>
2014-06-23 11:20:03 +0300

    Update upstream

--- tests/src/AbstractTestCase.php
+++ tests/src/AbstractTestCase.php
@@ -34,10 +34,10 @@ abstract class AbstractTestCase extends PHPUnit_Framework_TestCase {
 
         $this->logger = new TestLogger();
 
-        DB::setConfig('default', array(
+        DB::setConfig([
             'dsn' => 'mysql:dbname=harp-orm/timestamps;host=127.0.0.1',
             'username' => 'root',
-        ));
+        ]);
 
         DB::get()->setLogger($this->logger);
         DB::get()->beginTransaction();
